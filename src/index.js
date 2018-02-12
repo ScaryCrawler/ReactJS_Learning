4643954a0b75f62053439d41f3e6ea78ff1a2434
ScaryCrawler <sakhno.iv@gmail.com>
2018-02-12 18:52:32 +0300

    New skeleton project was implemented based on "create-react-app" package

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+    <h1 className="logo">
+        <span className="word1">React</span>
+        <span className="word2">Start</span>
+    </h1>,
+    document.getElementById("root")
+);
